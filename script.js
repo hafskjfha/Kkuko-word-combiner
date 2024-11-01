174f8f061c8d00d8d1770309803b724d19169387
s <jjj672604@gmail.com>
2024-11-01 12:53:18 +0900

    원상복구

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-import init, {CombinationManager} from './wasm/pkg/wasm.js';
+import CombinationManager from './CombinationsManger.js';
 let dictionary6 = [];
 let dictionary5 = [];
-async function __init__() {
-    await init();
-}
-__init__();
-/**
- * 
- * @param {str} str 
- * @param {Array<String>} words 
- * @returns {CombinationManager}
- */
-function initword(str,words){
-    const manager = new CombinationManager(str)
-    const chunkSize = 5000;
-    for (let i = 0; i < words.length; i += chunkSize) {
-        const chunk = words.slice(i, i + chunkSize);
-        manager.add_words(chunk);
-    }
-
-    return manager;
-}
 async function fetchTextFile6(url) {
     try {
         const response = await fetch(url);
@@ -158,16 +138,20 @@ function processHtml() {
 
     return {jswq, jjswq, jjjswq};
 }
-
+/**
+ * 
+ * @param {CombinationManager} manager1 
+ * @returns {Array<string[]>}
+ */
 function makedata(manager1) {
     const startTime = performance.now();
     let result6 = [];
-    if (manager1.has_possible_word()) {
-        while (manager1.has_possible_word()) {
+    if (manager1.hasPossibleWord()) {
+        while (manager1.hasPossibleWord()) {
             manager1.counts();
-            const best = manager1.get_best_and_remove();
+            const best = manager1.getBestAndRemove();
             result6.push(best);
-            manager1.find_possible_words();
+            manager1.findPossibleWords();
         }
 
     } else {
@@ -175,16 +159,15 @@ function makedata(manager1) {
         result6 = ['No possible words found.'];
     }
     console.log(result6.length);
-    //console.log(result6);
-    const manager2=initword(manager1.remainstr(),dictionary5)
-    manager2.find_possible_words();
+    const manager2=new CombinationManager(manager1.remainstr(),dictionary5);
+    manager2.findPossibleWords();
     let result5 = [];
-    if (manager2.has_possible_word()) {
-        while (manager2.has_possible_word()) {
+    if (manager2.hasPossibleWord()) {
+        while (manager2.hasPossibleWord()) {
             manager2.counts();
-            const best = manager2.get_best_and_remove();
+            const best = manager2.getBestAndRemove();
             result5.push(best);
-            manager2.find_possible_words();
+            manager2.findPossibleWords();
         }
 
     } else {
@@ -273,8 +256,8 @@ function outdata(WordList6, WordList5, remainingStringa, mode) {
 }
 
 function processing(mode, str) {
-    const manager=initword(str,dictionary6);
-    manager.find_possible_words();
+    const manager = new CombinationManager(str,dictionary6);
+    manager.findPossibleWords();
     const [wordList6, wordList5, remainingStringa] = makedata(manager);
     outdata(wordList6, wordList5, remainingStringa, mode);
 }
@@ -348,123 +331,6 @@ function submit3() {
     }, 1);
 }
 
-class CombinationManagerjs {
-    constructor(syllable = '', words = []) {
-        this.syllable = syllable;
-        this.words = words;
-        this.possibleWords = [];
-        this.letterCount = {};
-        this.wordCount = {};
-    }
-
-    getBestAndRemove() {
-        const best = this.getBest();
-        this.deleteWord(best);
-        return best;
-    }
-
-    deleteWord(word) {
-        let deleted = this.syllable;
-        for (const char of word) {
-            deleted = deleted.replace(char, '');
-        }
-        this.syllable = deleted;
-    }
-
-    getBest() {
-        return Object
-            .keys(this.wordCount)
-            .reduce(
-                (a, b) => this.wordCount[a] < this.wordCount[b]
-                    ? a
-                    : b
-            );
-    }
-
-    counts() {
-        this.countLetter();
-        this.countWord();
-    }
-
-    findPossibleWords() {
-        if (this.possibleWords.length === 0) {
-            for (const word of this.words) {
-                if (this.exist(this.syllable, this.insert(word))) {
-                    this
-                        .possibleWords
-                        .push(word);
-                }
-            }
-        } else {
-            this.fastFindPossibleWords();
-        }
-    }
-
-    hasPossibleWord() {
-        return this.possibleWords.length > 0;
-    }
-
-    countWord() {
-        this.wordCount = {};
-        for (const word of this.possibleWords) {
-            let count = 0;
-            for (const letter of word) {
-                count += this.letterCount[letter] || 0;
-            }
-            this.wordCount[word] = count;
-        }
-    }
-
-    countLetter() {
-        this.letterCount = {};
-        for (const word of this.words) {
-            for (const letter of word) {
-                this.letterCount[letter] = (this.letterCount[letter] || 0) + 1;
-            }
-        }
-    }
-
-    fastFindPossibleWords() {
-        const temp = [];
-        for (const word of this.possibleWords) {
-            if (this.exist(this.syllable, this.insert(word))) {
-                temp.push(word);
-            }
-        }
-        this.possibleWords = temp;
-    }
-
-    insert(arr1) {
-        const arr = arr1.split('');
-        for (let i = 1; i < arr.length; i++) {
-            const standard = arr[i];
-            let aux = i - 1;
-            while (aux >= 0 && standard < arr[aux]) {
-                arr[aux + 1] = arr[aux];
-                aux--;
-            }
-            arr[aux + 1] = standard;
-        }
-        return arr.join('');
-    }
-
-    exist(syllable, word) {
-        let count = 0;
-        for (const s of syllable) {
-            if (s === word[count]) {
-                count++;
-            }
-            if (count === word.length) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    remainstr() {
-        return this.syllable
-    }
-}
 
 window.submit1 = submit1;
 window.submit2 = submit2;
